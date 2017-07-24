d06b85ee9a8e425618c6a35df0b1e6482dcf8512
WillBatchelor <batchelorwd8@gmail.com>
2017-07-24 10:38:36 -0400

    Got basic digital clockface working.

--- src/c/botw-watchface.c
+++ src/c/botw-watchface.c
@@ -5,6 +5,21 @@ static Window *s_main_window;
 
 static TextLayer *s_time_layer;
 
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+
 static void main_window_load(Window *window) {
 Layer *window_layer = window_get_root_layer(window);
 GRect bounds = layer_get_bounds(window_layer);	
@@ -14,16 +29,22 @@ s_time_layer = text_layer_create(
 	
 text_layer_set_background_color(s_time_layer, GColorClear);
 text_layer_set_text_color(s_time_layer, GColorBlack);
-text_layer_set_text(s_time_layer, "00:00");
 text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
 text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 	
 layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+
+update_time();
 }
 
 static void main_window_unload(Window *window) {
 	text_layer_destroy(s_time_layer);	
 }
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+	update_time();	
+}
+
 
 static void init() {
 	s_main_window = window_create();
@@ -32,8 +53,10 @@ static void init() {
 		.load = main_window_load,
 		.unload = main_window_unload 
 	});
+	
+	window_stack_push(s_main_window, true);
 
-window_stack_push(s_main_window, true);
+	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);	
 }
 
 static void deinit() {
