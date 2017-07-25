a4b9487e93c7d2dacca4d02483c4869c2a784ba8
WillBatchelor <batchelorwd8@gmail.com>
2017-07-24 21:16:49 -0400

    Starting to add new textlayer for hexadecimal.

--- src/c/botw-watchface.c
+++ src/c/botw-watchface.c
@@ -5,6 +5,8 @@ static Window *s_main_window;
 
 static TextLayer *s_time_layer;
 
+//static Textlayer
+
 static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL);
@@ -12,8 +14,7 @@ static void update_time() {
 
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
@@ -28,7 +29,7 @@ s_time_layer = text_layer_create(
 	GRect(4, PBL_IF_ROUND_ELSE(8, 2), bounds.size.w, 50));
 	
 text_layer_set_background_color(s_time_layer, GColorBlack);
-text_layer_set_text_color(s_time_layer, GColorPictonBlue);
+text_layer_set_text_color(s_time_layer, GColorCyan);
 text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
 text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
 	
