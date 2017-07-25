7d46459ce6affa2c7b2b75535ea5d9d8709f034c
WillBatchelor <batchelorwd8@gmail.com>
2017-07-24 22:48:33 -0400

    Attempted to add AM and PM.

--- src/c/botw-watchface.c
+++ src/c/botw-watchface.c
@@ -14,7 +14,7 @@ static void update_time() {
 
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M %p", tick_time);
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M %p" : "%I:%M %p", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
