66ee8ec1b51c559f1ef637026db947e186dc2b1e
WillBatchelor <batchelorwd8@gmail.com>
2017-07-25 09:01:42 -0400

    Got rid of AM/PM.
    I think it is beyond the parameters of the Pebble SDK as far as I understand it.

--- src/c/botw-watchface.c
+++ src/c/botw-watchface.c
@@ -15,8 +15,8 @@ static void update_time() {
   struct tm *tick_time = localtime(&temp);
 
   // Write the current hours and minutes into a buffer
-  static char s_buffer[22];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M %p", tick_time);
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
