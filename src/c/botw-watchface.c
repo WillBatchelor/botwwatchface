a6e98ecc8864df5bb7b87c58a175c0fce9d3ef77
WillBatchelor <batchelorwd8@gmail.com>
2017-08-02 10:17:44 -0400

    Added date textlayer. Need to debug.

--- src/c/botw-watchface.c
+++ src/c/botw-watchface.c
@@ -6,6 +6,7 @@
 static Window *s_main_window;
 
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 
 static TextLayer *s_time_hex_layer;
 static GFont s_hex_font;
@@ -18,16 +19,20 @@ static void update_time() {
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
-
-unsigned int h;
-	h=(unsigned int)time(NULL); 
+	
+	unsigned int h;
+		h=(unsigned int)time(NULL); 
 	
 static char s_hext_buffer[20];
 snprintf(s_hext_buffer, 20, "%X", h); 
 
+static char s_date_buffer[20];
+	strftime(s_date_buffer, sizeof(s_date_buffer), "%a %b %d", tick_time);
+	
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
 	text_layer_set_text(s_time_hex_layer, s_hext_buffer);
+	text_layer_set_text(s_date_layer, s_date_buffer);
 }
 
 
@@ -38,27 +43,35 @@ GRect bounds = layer_get_bounds(window_layer);
 s_time_layer = text_layer_create(
 	GRect(0, 2, bounds.size.w, 44));
 	
-text_layer_set_background_color(s_time_layer, GColorClear);
-text_layer_set_text_color(s_time_layer,GColorPictonBlue);
-text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
-text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
+	text_layer_set_background_color(s_time_layer, GColorClear);
+	text_layer_set_text_color(s_time_layer,GColorPictonBlue);
+	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
+	text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
 
 s_hex_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SC_BOLD_12));
 	
 s_time_hex_layer = text_layer_create(
 	GRect(0, 48, bounds.size.w, 14));
 	
-text_layer_set_background_color(s_time_hex_layer, GColorClear);
-text_layer_set_text_color(s_time_hex_layer, GColorCobaltBlue);
-text_layer_set_font(s_time_hex_layer, s_hex_font);
-text_layer_set_text_alignment(s_time_hex_layer, GTextAlignmentLeft);
-//text_layer_set_text(s_time_hex_layer, "This is a test...");
+	text_layer_set_background_color(s_time_hex_layer, GColorClear);
+	text_layer_set_text_color(s_time_hex_layer, GColorCobaltBlue);
+	text_layer_set_font(s_time_hex_layer, s_hex_font);
+	text_layer_set_text_alignment(s_time_hex_layer, GTextAlignmentLeft);
 
+s_date_layer = text_layer_create(
+	GRect(0, 52, bounds.size.w, 44));
+	
+	text_layer_set_background_color(s_date_layer, GColorClear);
+	text_layer_set_text_color(s_date_layer,GColorPictonBlue);
+	text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
+	text_layer_set_text_alignment(s_date_layer, GTextAlignmentLeft);
+
+	
 layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
 layer_add_child(window_layer, text_layer_get_layer(s_time_hex_layer));
 
-
+layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 	
 update_time();
 	
@@ -68,6 +81,7 @@ static void main_window_unload(Window *window) {
 	text_layer_destroy(s_time_layer);
 	fonts_unload_custom_font(s_hex_font);
 	text_layer_destroy(s_time_hex_layer);
+	text_layer_destroy(s_date_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
