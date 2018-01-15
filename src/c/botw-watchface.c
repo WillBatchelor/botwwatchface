4f81eb9740218a9a9e0656f556d0adb3ede303ca
WillBatchelor <batchelorwd8@gmail.com>
2018-01-14 21:30:44 -0500

    increased available characters for date font

--- src/c/botw-watchface.c
+++ src/c/botw-watchface.c
@@ -19,38 +19,40 @@ static void update_time() {
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
-	
-	unsigned int h;
-		h=(unsigned int)time(NULL); 
-	
-static char s_hext_buffer[20];
-snprintf(s_hext_buffer, 20, "%X", h); 
 
-static char s_date_buffer[20];
-	strftime(s_date_buffer, sizeof(s_date_buffer), "%a %b %d", tick_time);
+	unsigned int h;
+	
+	h=(unsigned int)time(NULL); 
+	
+  static char s_hext_buffer[20];
+  snprintf(s_hext_buffer, 20, "%X", h); 
+
+  static char s_date_buffer[20];
+	strftime(s_date_buffer, sizeof(s_date_buffer), "%a %m/%d" , tick_time);
 	
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
 	text_layer_set_text(s_time_hex_layer, s_hext_buffer);
 	text_layer_set_text(s_date_layer, s_date_buffer);
+	//text_layer_set_text(s_date_layer, "test");
 }
 
 
 static void main_window_load(Window *window) {
-Layer *window_layer = window_get_root_layer(window);
-GRect bounds = layer_get_bounds(window_layer);	
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_bounds(window_layer);	
 	
-s_time_layer = text_layer_create(
+	s_time_layer = text_layer_create(
 	GRect(0, 2, bounds.size.w, 44));
-	
+
 	text_layer_set_background_color(s_time_layer, GColorClear);
 	text_layer_set_text_color(s_time_layer,GColorPictonBlue);
 	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
 	text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
 
-s_hex_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SC_BOLD_12));
-	
-s_time_hex_layer = text_layer_create(
+	s_hex_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SC_BOLD_12));
+
+	s_time_hex_layer = text_layer_create(
 	GRect(0, 48, bounds.size.w, 14));
 	
 	text_layer_set_background_color(s_time_hex_layer, GColorClear);
@@ -58,22 +60,23 @@ s_time_hex_layer = text_layer_create(
 	text_layer_set_font(s_time_hex_layer, s_hex_font);
 	text_layer_set_text_alignment(s_time_hex_layer, GTextAlignmentLeft);
 
-s_date_layer = text_layer_create(
-	GRect(0, 52, bounds.size.w, 44));
+	s_date_layer = text_layer_create(
+		GRect(0, 56, bounds.size.w, 44));
 	
 	text_layer_set_background_color(s_date_layer, GColorClear);
 	text_layer_set_text_color(s_date_layer,GColorPictonBlue);
-	text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
+	//text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
+	text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
 	text_layer_set_text_alignment(s_date_layer, GTextAlignmentLeft);
 
 	
-layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+	layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
-layer_add_child(window_layer, text_layer_get_layer(s_time_hex_layer));
+	layer_add_child(window_layer, text_layer_get_layer(s_time_hex_layer));
 
-layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
-	
-update_time();
+	layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
+
+	update_time();
 	
 }
 
